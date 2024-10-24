af47ad28339f8f42cf3dce8b056622c0d6a59245
theo-be <belliere.theo@outlook.fr>
2024-10-25 00:28:36 +0200

    acceleration du perso

--- scripts/Perso.cs
+++ scripts/Perso.cs
@@ -3,16 +3,36 @@ using System;
 
 public partial class Perso : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -450f;
+
+	// private Vector2 WalkVelocity = Vector2.Zero;
+	
+	[Export]
+	public float DefaultSpeed = 200.0f;
+	[Export]
+	public float MaxSpeed = 400.0f;
+	
+	[Export]
+	public float JumpVelocity = -450f;
 
 	// Variable pour gérer le double saut
 	private int jumpCount = 0;
-	private const int maxJumps = 2;
+	
+	[Export]
+	public int maxJumps = 2;
+
+	[Export] 
+	public float HAcceleration = 5f;
+	
+	[Export]
+	public float HDeceleration = 40f;
+	
+	
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		bool canDash = false;
+		
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -27,21 +47,32 @@ public partial class Perso : CharacterBody2D
 		}
 
 		// Handle Jump (saut ou double saut)
-		if (Input.IsActionJustPressed("ui_accept") && jumpCount < maxJumps)
+		if (Input.IsActionJustPressed("saut") && jumpCount < maxJumps)
 		{
 			velocity.Y = JumpVelocity;
 			jumpCount++;  // Incrémente le nombre de sauts
 		}
 
 		// Get the input direction and handle the movement/deceleration.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("left", "right", "up", "down");
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			
+			velocity.X = direction.X * DefaultSpeed;
+
+			// velocity.X += HAcceleration * direction.X;
+
+
+			if (Mathf.Abs(velocity.X) < DefaultSpeed)
+				velocity.X = DefaultSpeed * direction.X;
+			else if (Mathf.Abs(velocity.X) > MaxSpeed)
+				velocity.X = MaxSpeed * direction.X;
+
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			// velocity.X = Mathf.MoveToward(Velocity.X, 0, HDeceleration);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, DefaultSpeed);
 		}
 
 		Velocity = velocity;
